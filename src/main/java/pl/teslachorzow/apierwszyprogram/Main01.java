23b3720b03054db0ff40ba647be09e67fc5fffb1
kurs <lbujocz@gmail.com>
2021-10-29 02:01:06 +1100

    Moj pierwszy commit2

---
+++ src/main/java/pl/teslachorzow/apierwszyprogram/Main01.java
@@ -0,0 +1,9 @@
+package pl.teslachorzow.apierwszyprogram;
+
+public class Main01 {
+        //pvsm
+    public static void main(String[] args) {
+        //sout
+        System.out.println("Mój pierwszy program w Java");
+    }
+}
